cc10c877c649b354e62fbce974f41a0d26ae04fa
Georg Kahest <georg@kahest.ee>
2021-12-15 02:05:33 +0200

    dice_sum

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,16 @@
-def main():
-  print('You rolled a die')
+import random
+dice_rolls = 2
+dice_sum_global = 0
 
-if __name__== "__main__":
-  main()
+
+def main(dice_sum=dice_sum_global):
+    for i in range(0, dice_rolls):
+
+        roll = random.randint(1, 6)
+        dice_sum += roll
+        print(f'You rolled a {roll}')
+    print(f'You have rolled a total of {dice_sum}')
+
+
+if __name__ == "__main__":
+    main()
